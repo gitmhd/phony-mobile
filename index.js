215a273f237ef10f7d9c2937b4f9850109dc4c11
Mouhammad Albissaty Saleh <git.mhd@gmail.com>
2026-09-21 06:08:37 +0300

    fix: polyfill URL for supabase realtime compatibility

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-/**
- * @format
- */
-
+import 'react-native-url-polyfill/auto'; // <-- Must be line 1
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
